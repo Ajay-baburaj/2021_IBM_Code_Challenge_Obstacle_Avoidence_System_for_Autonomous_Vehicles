3962a9b80d93d7637d1df81c6f012a8b4aa51e4d
Ajay-baburaj <96784826+Ajay-baburaj@users.noreply.github.com>
2022-05-05 17:49:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from rrt import RRT
 # import math
 # import pygame
 # import random
+
+import sqlite3
+path_coordinate_list = []
+
 if __name__ == '__main__':
     rrt = RRT((50,50),(930, 510), "ObstacleMap2.png", "DDR.png")
     running = True
@@ -23,3 +27,28 @@ if __name__ == '__main__':
 # f = open('flask_api/rrt_file.pkl','wb')
 # pickle.dump(rrt,f)
 # f.close()
+
+#connecting to database(sqlite)
+
+coordinate_list_main = path_coordinate_list[0]
+
+conn = sqlite3.connect('database.db')
+c = conn.cursor()
+ 
+
+def create_table():
+    c.execute('CREATE TABLE IF NOT EXISTS RecordONE (Xcordinate REAL, Ycordinate REAL)')
+ 
+def data_entry():
+    for i in range(len(coordinate_list_main)):
+        tuple_a = coordinate_list_main[i]
+        xcordinate = tuple_a[0]
+        ycordinate = tuple_a[1]
+        c.execute("INSERT INTO RecordONE (Xcordinate, Ycordinate) VALUES(?, ?)", (xcordinate, ycordinate))
+        conn.commit()
+ 
+create_table()
+data_entry()
+ 
+c.close()
+conn.close()
